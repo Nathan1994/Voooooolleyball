5f89352323835ff4067e9c3b7cf4baf33d876df2
Nathan <shaozeman@qq.com>
2015-06-21 15:36:43 +0800

    update for obstacle

--- Voooooolleyball/Classes/GamePlayerLayer.cpp
+++ Voooooolleyball/Classes/GamePlayerLayer.cpp
@@ -14,11 +14,14 @@
 
 USING_NS_CC;
 
-static const int BALL_TAG = 0x000;
-static const int PLAYER_TAG = 0x001;
+static const int BALL_TAG = 0x001;
+static const int PLAYER_TAG = 0x111;
 static const int WALL_TAG = 0x011;
-static const int GROUND_TAG = 0x010;
-static const int ENEMY_TAG = 0x100;
+static const int GROUND_TAG = 0x100;
+static const int ENEMY_TAG = 0x101;
+static const int OBSTACLE_TAG = 0x110;
+
+static bool isGameStart = false;
 
 static bool isRight = false;
 static bool isMove = false;
@@ -35,6 +38,8 @@ static int rightWins = 0;
 
 Label *leftWinLabel = nullptr;
 Label *rightWinLabel = nullptr;
+
+static bool isRightLastWin = false;
 
 #pragma mark - Init Method
 
@@ -147,9 +152,25 @@ void GamePlayerLayer::configureObstacle(){
     auto obstacle = Node::create();
     obstacle->setAnchorPoint(Point(0.5,0));
     obstacle->setPosition(Point(VisibleRect::bottom().x,VisibleRect::bottom().y+ VisibleRect::getVisibleRect().size.height * (1-offsetScale)));
-    obstacle->setPhysicsBody(PhysicsBody::createEdgeBox(Size(100,500)));
+    auto body = PhysicsBody::createEdgeBox(Size(100,500));
+    body->setContactTestBitmask(0);
+    body->setCategoryBitmask(WALL_TAG);
+    body->setCollisionBitmask(WALL_TAG);
+    obstacle->setPhysicsBody(body);
     
     this->addChild(obstacle);
+    
+    auto highObstacle = Node::create();
+    highObstacle->setAnchorPoint(Point(0.5,0));
+    highObstacle->setPosition(Point(VisibleRect::bottom().x,VisibleRect::bottom().y+ VisibleRect::getVisibleRect().size.height * (1-offsetScale)));
+    auto highObstacleBody = PhysicsBody::createEdgeBox(Size(100,VisibleRect::getVisibleRect().size.height *2));
+    highObstacleBody->setTag(OBSTACLE_TAG);
+    highObstacleBody->setContactTestBitmask(0);
+    highObstacleBody->setCategoryBitmask(OBSTACLE_TAG);
+    highObstacleBody->setCollisionBitmask(OBSTACLE_TAG);
+    highObstacle->setPhysicsBody(highObstacleBody);
+    this->addChild(highObstacle);
+    
     
 }
 
@@ -163,12 +184,18 @@ void GamePlayerLayer::configureBall(){
     ball->setScale(ballScale);
     auto body = PhysicsBody::createCircle(VisibleRect::getVisibleRect().size.height/12);
     body->setContactTestBitmask(1);
+    body->setCategoryBitmask(BALL_TAG);
+    body->setCollisionBitmask(BALL_TAG);
     body->setTag(BALL_TAG);
     body->setGravityEnable(false);
     ball->setPhysicsBody(body);
-//    ball->setPosition(Point(4*VisibleRect::getVisibleRect().size.width/5,VisibleRect::getVisibleRect().size.height - VisibleRect::getVisibleRect().size.height/6 ));
-    ball->setPosition(Point(VisibleRect::getVisibleRect().size.width/5,VisibleRect::getVisibleRect().size.height - VisibleRect::getVisibleRect().size.height/6 ));
-
+    if (isRightLastWin) {
+        ball->setPosition(Point(VisibleRect::getVisibleRect().size.width/5,VisibleRect::getVisibleRect().size.height - VisibleRect::getVisibleRect().size.height/6 ));
+    }
+    else{
+        ball->setPosition(Point(4*VisibleRect::getVisibleRect().size.width/5,VisibleRect::getVisibleRect().size.height - VisibleRect::getVisibleRect().size.height/6 ));
+    }
+    
     this->addChild(ball);
 
 
@@ -181,6 +208,9 @@ void GamePlayerLayer::configurePlayer(){
     player = GamePlayerSprite::create("Game_Player.png");
 //    player->setPosition(Point(VisibleRect::getVisibleRect().size.width/5,VisibleRect::getVisibleRect().size.height*(1-offsetScale)+1));
     player->getPhysicsBody()->setTag(PLAYER_TAG);
+    player->getPhysicsBody()->setContactTestBitmask(1);
+    player->getPhysicsBody()->setCategoryBitmask(PLAYER_TAG);
+    player->getPhysicsBody()->setCollisionBitmask(PLAYER_TAG);
     player->setPosition(Point(VisibleRect::getVisibleRect().size.width/5,VisibleRect::center().y));
 
     this->addChild(player);
@@ -194,6 +224,9 @@ void GamePlayerLayer::configureEnemy(){
     }
     enemy = GamePlayerSprite::create("Game_Enemy.png");
     enemy->getPhysicsBody()->setTag(ENEMY_TAG);
+    enemy->getPhysicsBody()->setContactTestBitmask(1);
+    enemy->getPhysicsBody()->setCategoryBitmask(ENEMY_TAG);
+    enemy->getPhysicsBody()->setCollisionBitmask(ENEMY_TAG);
     enemy->setPosition(Point(4 * VisibleRect::getVisibleRect().size.width/5,VisibleRect::center().y));
     this->addChild(enemy);
 
@@ -207,7 +240,8 @@ void GamePlayerLayer::configureEdge(){
     auto body = PhysicsBody::createEdgeBox(Size(VisibleRect::getVisibleRect().size.width,VisibleRect::getVisibleRect().size.height * offsetScale), PHYSICSBODY_MATERIAL_DEFAULT, 3);
     body->setContactTestBitmask(1);
     body->setTag(WALL_TAG);
-    body->getShape(0)->setRestitution(0);
+    body->setCategoryBitmask(WALL_TAG);
+    body->setCollisionBitmask(WALL_TAG);
     wall->setPhysicsBody(body);
     this->addChild(wall);
     
@@ -216,6 +250,8 @@ void GamePlayerLayer::configureEdge(){
     auto groundBody = PhysicsBody::createEdgeBox(Size(VisibleRect::getVisibleRect().size.width,2), PHYSICSBODY_MATERIAL_DEFAULT, 3);
     groundBody->setContactTestBitmask(1);
     groundBody->setTag(GROUND_TAG);
+    groundBody->setCategoryBitmask(WALL_TAG);
+    groundBody->setCollisionBitmask(WALL_TAG);
     groundBody->getShape(0)->setRestitution(0);
     ground->setPhysicsBody(groundBody);
     this->addChild(ground);
@@ -228,7 +264,7 @@ void GamePlayerLayer::configureEdge(){
 
 void GamePlayerLayer::hitBall(){
     
-    if ((VisibleRect::getVisibleRect().size.width/2 - player->getPosition().x > 0)) {
+    if ((VisibleRect::center().x - ball->getPosition().x > 0)) {
         ball->getPhysicsBody()->setVelocity(Vec2(1000,-300));
     }
     else{
@@ -237,6 +273,7 @@ void GamePlayerLayer::hitBall(){
 }
 
 void GamePlayerLayer::restart(){
+    isGameStart = false;
     configurePlayer();
     configureEnemy();
     configureBall();
@@ -246,6 +283,7 @@ void GamePlayerLayer::restart(){
 
 void GamePlayerLayer::start(){
     ball->getPhysicsBody()->setGravityEnable(true);
+    isGameStart = true;
 }
 
 void GamePlayerLayer::showReady(){
@@ -331,9 +369,11 @@ bool GamePlayerLayer::onContactBegin(PhysicsContact& contact){
         if (a->getTag() == GROUND_TAG || b->getTag() == GROUND_TAG) {
             if (ball->getPosition().x - VisibleRect::center().x < 0) {
                 rightWins++;
+                isRightLastWin = true;
             }
             else{
                 leftWins++;
+                isRightLastWin = false;
             }
             this->runAction(Sequence::create(DelayTime::create(1),CallFuncN::create(CC_CALLBACK_0(GamePlayerLayer::restart,this)), NULL));
         }
@@ -404,7 +444,10 @@ void GamePlayerLayer::onTouchEnded(cocos2d::Touch *touch, cocos2d::Event *unused
 #pragma mark - Util
 
 void GamePlayerLayer::updateEnemy(){
-    
+    if (!isGameStart) {
+        isEnemyMove = false;
+        return;
+    }
 
     if (fabsf(ball->getPosition().x - enemy->getPosition().x) < 50) {
         enemy->jump();
